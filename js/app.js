e24cf39a0334b7cd86cec24c4b04c6953ce49a5f
stoicsleuth <tarashnkr321@gmail.com>
2018-03-25 12:12:44 +0530

    responsive

--- js/app.js
+++ js/app.js
@@ -2,7 +2,17 @@ const footerJump = document.querySelector('.footerJump');
 const toggle = document.querySelector('#toggle');
 if(footerJump){
     footerJump.addEventListener('click', ()=>{
-        console.log('Oo');
+       
         toggle.click();
     })
 }
+toggle.addEventListener('click', (e)=>{
+    console.log(e.target.checked);
+    if(e.target.checked)
+    {
+        document.querySelector('body').style['overflow-y'] = "hidden";
+    }
+    else{
+        document.querySelector('body').style['overflow-y'] = "visible";
+    }
+})
